50cddc8d2b0aee225c4431a809dc6ecce22178da
tuan123123 <125374828+tuan123123@users.noreply.github.com>
2025-12-04 00:38:04 -0600

    Remove unused imports from main.py
    Removed unused imports for ChatAnthropic and ChatPromptTemplate.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_agent
 from tools import search_tool, wiki_tool, save_tool
